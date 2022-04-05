2359f25dd1c0c1360ae1fa516f9cbdc9440748db
DaimonSh <dimaprotsap@gmail.com>
2022-04-05 17:01:19 +0300

    Added Shell sort

---
+++ src/daimonix/algorithms/sorting/ShellSorter.java
@@ -0,0 +1,23 @@
+package daimonix.algorithms.sorting;
+
+import java.util.Comparator;
+
+public class ShellSorter extends Sorter {
+
+    @Override
+    public <T> void Sort(T[] array, Comparator<? super T> comparator) {
+        int distance = array.length / 2;
+        while (distance > 0) {
+            for (int i = distance; i < array.length; i++) {
+                T temp = array[i];
+                int j = i;
+                while (j >= distance && comparator.compare(array[j - distance], temp) > 0) {
+                    array[j] = array[j - distance];
+                    j -= distance;
+                }
+                array[j] = temp;
+            }
+            distance /= 2;
+        }
+    }
+}
